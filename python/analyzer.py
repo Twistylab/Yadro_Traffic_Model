9563c8a127085b194815630f0f8b64c4beacd512
Nikita Zotov <nikita.zotov@metalab.ifmo.ru>
2026-05-10 16:09:46 +0300

    Add python analyzer

---
+++ python/analyzer.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import json
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def traffic_model_analyzer(df):
+    df["delay"] = df["time"].diff()
+
+    analysis_dict = dict()
+
+    analysis_dict["avg_package_size"] = df["size"].mean()
+    analysis_dict["avg_delay"] = df["delay"].mean()
+    analysis_dict["bitrate"] = df["size"].sum() / (df["time"].max() - df["time"].min())
+
+    if round(df["delay"].diff().sum()) == 0:
+        analysis_dict["model"] = "equal"
+    else:
+        analysis_dict["model"] = "poisson"
+
+    fig = plt.figure(figsize=(12, 10))
+
+    ax1 = fig.add_subplot(2, 1, 1)
+
+    df['delay'].hist(bins=30)
+    ax1.set_title('Гистограмма задержек между пакетами')
+    ax1.set_xlabel('Задержка')
+    ax1.set_ylabel('Частота')
+
+    ax2 = fig.add_subplot(2, 1, 2)
+
+    df['size'].hist(bins=30)
+    ax2.set_title('Гистограмма размеров пакетов')
+    ax2.set_xlabel('Размер')
+    ax2.set_ylabel('Частота')
+
+    return fig, analysis_dict
+
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+    data_frame = pd.read_csv(path)
+
+    result_dir = os.path.dirname(path)
+
+    hist, analysis_result =  traffic_model_analyzer(data_frame)
+
+    hist.savefig(os.path.join(result_dir, "histograms.png"), dpi=300)
+
+    with open(os.path.join(result_dir, "analysis.json"), "w") as file:
+        json.dump(analysis_result, file, indent=4)
+
